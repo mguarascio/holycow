f0d93d65c7ccd29c9a966d4ad9b638baf0bb5656
Michael Guarascio <michael.guarascio@gmail.com>
2013-03-27 20:57:14 -0400

    installer and more documentation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='holycow',
+      version='0.1',
+      description='ESPN Baseball API library for python',
+      author='Mike Guarascio',
+      author_email='michael.guarascio@gmail.com',
+      url='https://github.com/mguarascio/holycow.git',
+      packages=['requests'],
+     )
